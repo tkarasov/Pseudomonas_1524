a22df3ce894639087198a01d5d263967e171ec7a
tkarasov <tkarasov@gmail.com>
2018-06-22 12:10:04 +0200

    Update raxml_no_recombine_107.sh

--- scripts/recombination_dating/raxml_no_recombine_107.sh
+++ scripts/recombination_dating/raxml_no_recombine_107.sh
@@ -4,16 +4,9 @@
 #$ -pe parallel 20
 #  Request 640G of RAM
 #$ -l h_vmem=32G
-#
-#  The name shown in the qstat output and in the output file(s). The
-#  default is to use the script name.
-#$ -N $letter$number
-#
-#  The path used for the standard output stream of the job
+#  
 #$ -o $HOME/tmp/stdout_of_job
 #
-# Merge stdout and stderr. The job will create only one output file which
-# contains both the real output and the error messages.
 #$ -j y
 #
 #  Use /bin/bash to execute this script
@@ -21,8 +14,6 @@
 #
 #  Run job from current working directory
 #$ -cwd
-
-
 
 cd /ebio/abt6_projects9/Pseudomonas_diversity/data/post_assembly_analysis/pan_genome/data/vis/clonalframe
 /usr/bin/raxmlHPC-PTHREADS-SSE3  -T 20 -p 654329  -s concat_107_no_recom.fasta  -m GTRGAMMA -n concat_107_no_recom
